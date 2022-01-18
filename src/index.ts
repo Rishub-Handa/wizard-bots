af637d697edc7129ac0371c6966a3284c919f56a
Rishub-Handa <rh4gh@virginia.edu>
2022-01-17 20:44:57 -0500

    tweets when auction is about to end and closing prices

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,75 @@
-import { getAuctionClosingPriceMessage, getAuctionEndingMessage } from './messages'
+import { getAuctionClosingPriceMessage, getAuctionEndingMessage } from './messages';
+import { getAuctionInfo, getClosingPrices } from './auctionHouseSubscriber';
+import fs from 'fs';
+import path from 'path';
+
 import dotenv from 'dotenv';
+import TwitterApi from 'twitter-api-v2';
+
+dotenv.config();
+
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY || '',
+  appSecret: process.env.TWITTER_API_SECRET || '',
+  accessToken: process.env.TWITTER_ACCESS_TOKEN || '',
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET || ''
+})
+
+const MILLISECONDS_IN_23_HOURS = (23 * 60 * 60 * 1000);
+const CACHE_PATH = path.resolve(__dirname, "../store/cache.json")
+
+// async function tweet() {
+//   const tweet = await twitterClient.v1.tweet('messing with twtr api');
+//   console.log("tweet: ", tweet.id_str, tweet.full_text);
+//   const deletedTweet = await twitterClient.v1.deleteTweet(tweet.id_str);
+//   console.log('Deleted tweet', deletedTweet.id_str, ':', deletedTweet.full_text);
+// }
+
+
 
 async function botTick() {
   console.log("bot awakens")
 
-  console.log(getAuctionClosingPriceMessage(1, 2, 3.5, 5, 3, 5));
+  let cache = JSON.parse(fs.readFileSync(CACHE_PATH, 'utf-8'));
+
+  const { minutesLeft, wizardIds } = await getAuctionInfo();
+  console.log("minutesLeft: ", minutesLeft);
+  console.log("wizardIds: ", wizardIds);
+
+  // Make sure this tweet hasn't already been sent for this auction (> 23hrs ago)
+  const timeSinceLastAuctionEndingTweet = (new Date()).getTime() - cache.lastTimeSentAuctionEndingSoon;
+  console.log("time since last auction ending tweet", timeSinceLastAuctionEndingTweet);
+
+  if (minutesLeft <= 15 && timeSinceLastAuctionEndingTweet > MILLISECONDS_IN_23_HOURS) {
+    console.log('Tweeting auction ending soon');
+    // DEV: uncomment 
+    // twitterClient.v1.tweet(getAuctionEndingMessage());
+
+    // Update the last time this tweet was sent 
+    cache.lastTimeSentAuctionEndingSoon = (new Date()).getTime();
+    console.log(cache);
+  }
+
+  // If the wizardIds are different than what's in the cache, send out closing price tweet 
+  if (JSON.stringify(wizardIds.sort()) !== JSON.stringify(cache.wizardIds.sort())) {
+    console.log("new auction has started. sending out closing prices tweet");
+
+    const closingPrices = await getClosingPrices();
+    console.log("closing prices: ", closingPrices);
+    const closingPricesMsg = getAuctionClosingPriceMessage(wizardIds[0], closingPrices);
+
+    // DEV: uncomment 
+    // twitterClient.v1.tweet(getAuctionEndingMessage());
+    cache.lastTimeSentClosingPrices = (new Date()).getTime();
+    cache.wizardIds = wizardIds;
+  }
+
+
+  fs.writeFileSync(CACHE_PATH, JSON.stringify(cache));
 
 }
 
+botTick();
 
+// DEV: uncomment 
 // setInterval(async () => botTick(), 3000);
--- src/messages.ts
+++ src/messages.ts
@@ -4,12 +4,12 @@ export const getAuctionEndingMessage = () =>
   "The auction is ending soon! Get your bid in at https://wizardsdao.com";
 
 const wizardEmoji = emoji.get('mage')
-export const getAuctionClosingPriceMessage = (wizStartId: number, p1: number, p2: number, p3: number, p4: number, p5: number) =>
+export const getAuctionClosingPriceMessage = (wizStartId: number, bids: number[]) =>
   `AUCTION FINAL RESULTS
   
-  ${wizardEmoji} #${wizStartId}: ${p1} ETH
-  ${wizardEmoji} #${wizStartId + 1}: ${p2} ETH
-  ${wizardEmoji} #${wizStartId + 2}: ${p3} ETH
-  ${wizardEmoji} #${wizStartId + 3}: ${p4} ETH
-  ${wizardEmoji} #${wizStartId + 4}: ${p5} ETH
+  ${wizardEmoji} #${wizStartId}: ${bids[0]} ETH
+  ${wizardEmoji} #${wizStartId + 1}: ${bids[1]} ETH
+  ${wizardEmoji} #${wizStartId + 2}: ${bids[2]} ETH
+  ${wizardEmoji} #${wizStartId + 3}: ${bids[3]} ETH
+  ${wizardEmoji} #${wizStartId + 4}: ${bids[4]} ETH
   `
